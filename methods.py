e63c0be458bfea9fc8b23daf903d6a5041c179ff
louisbank <156777035+louisbank-edu-thailand@users.noreply.github.com>
2024-01-31 22:09:43 +0700

    Initial commit

---
+++ methods.py
@@ -0,0 +1,9 @@
+def is_palindrome(s: str) -> bool:
+    return s == s[::-1]
+
+
+def fibonacci(n: int) -> int:
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n - 2) + fibonacci(n - 1)
